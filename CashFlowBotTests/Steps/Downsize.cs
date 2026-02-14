c085356761247fb2a8deba762db18c75e1e98dca
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2026-02-14 10:52:03 +0200

    Add Downsize e2e tests

---
+++ CashFlowBotTests/Steps/Downsize.cs
@@ -0,0 +1,14 @@
+﻿using CashFlowBotTests.Extras;
+using TechTalk.SpecFlow;
+
+namespace CashFlowBotTests.Steps;
+
+[Binding]
+public class Downsize(StepsContext context)
+{
+    private readonly StepsContext _context = context;
+    private User User => _context.User;
+
+    [When("I lost my job")]
+    public void LostJob() => User.SendMessage("Downsize");
+}
