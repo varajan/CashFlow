1448c77f8372fb170907814d0a6de6cf06de1046
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2026-02-15 10:04:42 +0200

    Add 'Charity' e2e tests

---
+++ CashFlowBotTests/Steps/CharitySteps.cs
@@ -0,0 +1,18 @@
+﻿using CashFlowBotTests.Extras;
+using TechTalk.SpecFlow;
+
+namespace CashFlowBotTests.Steps;
+
+[Binding]
+public class CharitySteps(StepsContext context)
+{
+    private readonly StepsContext _context = context;
+    private User User => _context.User;
+
+    [When("I donate to a charity")]
+    public void Charity()
+    {
+        User.SendMessage("Show my data");
+        User.SendMessage("Charity - Pay 10%");
+    }
+}
