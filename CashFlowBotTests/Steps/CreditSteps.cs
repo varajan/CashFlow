3d6751193910bcf07d580de1bdfd3c2e6ba9ee75
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2026-02-15 09:54:56 +0200

    Add 'get credit' e2e tests

---
+++ CashFlowBotTests/Steps/CreditSteps.cs
@@ -0,0 +1,19 @@
+﻿using CashFlowBotTests.Extras;
+using TechTalk.SpecFlow;
+
+namespace CashFlowBotTests.Steps;
+
+[Binding]
+public class CreditSteps(StepsContext context)
+{
+    private readonly StepsContext _context = context;
+    private User User => _context.User;
+
+    [When("I get (.*) as a credit")]
+    public void GetCredit(string amount)
+    {
+        User.SendMessage("Show my data");
+        User.SendMessage("Get credit");
+        User.SendMessage(amount);
+    }
+}
