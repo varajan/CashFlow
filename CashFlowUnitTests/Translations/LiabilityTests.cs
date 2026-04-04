56f98c7e1510a935da43239398ed739746750f18
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2026-04-04 20:41:14 +0300

    Add LiabilityTests

---
+++ CashFlowUnitTests/Translations/LiabilityTests.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Data.Consts;
+using CashFlow.Extensions;
+using System.Reflection;
+
+namespace CashFlowUnitTests.Translations;
+
+[TestFixture]
+public class LiabilityTests
+{
+    public static IEnumerable<Liability> Liabilities => Enum.GetValues<Liability>();
+
+    private readonly List<string?> TermValues = typeof(Terms)
+        .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+        .Where(f => f.IsLiteral && !f.IsInitOnly)
+        .Select(f => (string?)f.GetValue(null))
+        .Where(v => v is not null)
+        .ToList();
+
+    [TestCaseSource(nameof(Liabilities))]
+    public void Liability_has_translation(Liability liability) =>
+        Assert.That(TermValues, Does.Contain(liability.GetDescription()));
+
+    [Test]
+    public void Liabilities_have_distinct_translations() =>
+        Assert.That(Liabilities.Select(at => at.GetDescription()), Is.Unique);
+}
