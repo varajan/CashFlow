44998750f344800db3722fb7d8f4adb31c4977c2
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2026-04-04 20:44:57 +0300

    Terms unit tests

--- CashFlowUnitTests/Translations/ActionTypeTests.cs
+++ CashFlowUnitTests/Translations/ActionTypeTests.cs
@@ -1,20 +1,12 @@
 ﻿using CashFlow.Data.Consts;
 using CashFlow.Extensions;
-using System.Reflection;
 
 namespace CashFlowUnitTests.Translations;
 
 [TestFixture]
-public class ActionTypeTests
+public class ActionTypeTests : BaseTest
 {
     public static IEnumerable<ActionType> ActionTypes => Enum.GetValues<ActionType>();
-
-    private readonly List<string?> TermValues = typeof(Terms)
-        .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
-        .Where(f => f.IsLiteral && !f.IsInitOnly)
-        .Select(f => (string?)f.GetValue(null))
-        .Where(v => v is not null)
-        .ToList();
 
     [TestCaseSource(nameof(ActionTypes))]
     public void ActionType_has_translation(ActionType actionType) =>
--- CashFlowUnitTests/Translations/LiabilityTests.cs
+++ CashFlowUnitTests/Translations/LiabilityTests.cs
@@ -1,20 +1,12 @@
 ﻿using CashFlow.Data.Consts;
 using CashFlow.Extensions;
-using System.Reflection;
 
 namespace CashFlowUnitTests.Translations;
 
 [TestFixture]
-public class LiabilityTests
+public class LiabilityTests : BaseTest
 {
     public static IEnumerable<Liability> Liabilities => Enum.GetValues<Liability>();
-
-    private readonly List<string?> TermValues = typeof(Terms)
-        .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
-        .Where(f => f.IsLiteral && !f.IsInitOnly)
-        .Select(f => (string?)f.GetValue(null))
-        .Where(v => v is not null)
-        .ToList();
 
     [TestCaseSource(nameof(Liabilities))]
     public void Liability_has_translation(Liability liability) =>
