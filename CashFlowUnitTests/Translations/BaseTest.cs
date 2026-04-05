3399c12d385255cf2f83b4c4feaedf001e7026b3
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2026-04-05 18:05:54 +0300

    Add Language_has_all_translations test

--- CashFlowUnitTests/Translations/BaseTest.cs
+++ CashFlowUnitTests/Translations/BaseTest.cs
@@ -5,7 +5,9 @@ namespace CashFlowUnitTests.Translations;
 
 public class BaseTest
 {
-    protected readonly List<string?> TermValues = typeof(Terms)
+    public static IEnumerable<Language> Languages => Enum.GetValues<Language>();
+
+    public static readonly List<string?> TermValues = typeof(Terms)
         .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
         .Where(f => f.IsLiteral && !f.IsInitOnly)
         .Select(f => (string?)f.GetValue(null))
---
+++ CashFlowUnitTests/Translations/LanguageTests.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Data.Consts;
+using CashFlow.Data.Services;
+
+namespace CashFlowUnitTests.Translations;
+
+[TestFixtureSource(typeof(BaseTest), nameof(Languages))]
+public class LanguageTests(Language language) : BaseTest
+{
+    private readonly TranslationService TranslationService = new();
+
+    private readonly string[] Params = ["0", "1", "2", "3", "4"];
+
+    [TestCaseSource(typeof(BaseTest), nameof(TermValues))]
+    public void Language_has_all_translations(string term) =>
+        Assert.That(TranslationService.Get(term, language, Params),
+            Is.Not.Null.And.Not.Empty);
+}
