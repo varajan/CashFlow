fcd23bb00554c21012bb11080b844a642911b830
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2026-04-19 09:31:44 +0300

    Move classes to their files

---
+++ CashFlow/Data/Consts/BuyPrice.cs
@@ -0,0 +1,14 @@
+﻿namespace CashFlow.Data.Consts;
+
+public class BuyPrice
+{
+    public static readonly int[] Business = [20000, 25000, 30000, 50000, 100000, 125000, 150000, 200000, 350000, 500000];
+    public static readonly int[] BusinessSmall = [3000, 5000];
+    public static readonly int[] BusinessBig = [100000, 120000, 150000, 200000, 225000, 250000, 300000];
+    public static readonly int[] RealEstateSmall = [35000, 40000, 45000, 50000, 55000, 60000, 65000];
+    public static readonly int[] RealEstateBig = [45000, 50000, 60000, 65000, 67000, 70000, 75000, 80000, 90000, 100000, 115000, 125000, 140000, 150000, 160000, 200000, 220000, 240000, 350000, 550000, 575000, 1200000];
+    public static readonly int[] Stock = [1, 5, 10, 20, 30, 40, 50, 1200, 4000, 5000];
+    public static readonly int[] Land = [5000, 10000, 20000];
+    public static readonly int[] Coin = [300, 500];
+    public static readonly int[] BigDream = [100_000, 125_000, 150_000, 175_000, 200_000, 225_000, 250_000, 300_000];
+}
---
+++ CashFlow/Data/Consts/FirstPayment.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Data.Consts;
+
+public class FirstPayment
+{
+    public static readonly int[] Business = [20000, 25000, 30000, 40000, 50000, 100000];
+    public static readonly int[] RealEstateSmall = [0, 2000, 3000, 4000, 5000];
+    public static readonly int[] RealEstateBig = [6000, 7000, 8000, 9000, 10000, 12000, 15000, 16000, 20000, 30000, 32000, 40000, 50000, 75000, 200000];
+}
